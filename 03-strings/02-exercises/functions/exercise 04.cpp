9546ef7d513eedbbf3f372be4a71223e6e205648
Sharan Kataria <sharankataria38@gmail.com>
2025-03-13 00:49:17 +0500

    Add files via upload

---
+++ 03-strings/02-exercises/functions/exercise 04.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+//	Function at().
+
+	int index;
+	string s1;
+	cout<<"Enter a string:"<<endl;
+	cin>>s1;
+	cout<<"Enter a index:"<<endl;
+	cin>>index;
+	cout<<"character to index:"<<endl;
+	cout<<s1.at(index)<<endl;
+
+
+}
