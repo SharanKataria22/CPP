5dae4523c7cea5e60e46789ff189ff4b1f511314
Sharan Kataria <sharankataria38@gmail.com>
2025-03-22 19:06:20 +0500

    Add files via upload

---
+++ 03-strings/Sharan's Website/Untitled123.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+//	cout<<"This is Veriable Syntax"
+	cout<<"This is Veriable Syntax"<<endl;
+	int x=5;
+	cout<< x <<endl;
+	int y=8;
+	cout<< y <<endl;
+	
+	float pi=5.316;
+	cout<<pi<<endl;
+	float py=1754.54;
+	cout<<py<<endl;
+}
