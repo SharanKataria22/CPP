dd1a692729523090223004c97b13973320b984b5
Sharan Kataria <sharankataria38@gmail.com>
2025-04-08 01:07:43 +0500

    Create basic_structure.cpp

---
+++ 00-introduction/basic_structure.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+// Program showing structure of a basic C++ code
+int main() {
+    // Code starts from here
+    cout << "This is a basic structure example." << endl;
+    return 0; // Program ends
+}
