7f2499d8890e8a1a5464d6b5bb8f394e91738e66
Sharan Kataria <sharankataria38@gmail.com>
2025-04-08 01:08:20 +0500

    Create comments.cpp

---
+++ 00-introduction/comments.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// This is a single-line comment
+
+/*
+ This is a 
+ multi-line comment
+*/
+
+int main() {
+    cout << "Comment demo" << endl;
+    return 0;
+}
